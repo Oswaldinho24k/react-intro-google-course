d57a523849bb825983a9e211e39291f5a1e0d80e
oswaldo  martinez <oswaldomartinez@MacBook-Pro-de-oswaldo.local>
2019-04-02 00:03:44 -0600

    Demo DOne :sunglasses:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import icon1 from './images/icon1.png'
+import icon2 from './images/icon2.png'
+import icon3 from './images/icon3.png'
+import icon4 from './images/icon4.png'
+import react from './images/react-logo.svg'
+import menu from './images/menu-top.svg'
+import ironhack from './images/ironhack-logo.svg'
+
 
 
 class App extends React.Component {
@@ -8,8 +15,12 @@ class App extends React.Component {
       <div>
         <section className="main__section">
           <nav className="navbar">
-            <span>LOGO</span>
-            <span>Menu</span>
+            <span>
+              <img src={ironhack} alt="ih" />
+            </span>
+            <span>
+              <img src={menu} alt="menu" />
+            </span>
           </nav>
           <article className="article__texts">
             <h1>Say Hello to ReactJS</h1>
@@ -19,26 +30,29 @@ class App extends React.Component {
         </section>
         <section className="cards__section">
           <div>
-            <img src={icon1} />
+            <img src={icon1} alt="logo" />
             <h3>Declarative</h3>
             <p>  React makes it painless to create  interactive UI's </p>
           </div>
           <div>
-            <img src={icon1} />
+            <img src={icon2} alt="logo" />
             <h3>Declarative</h3>
             <p>  React makes it painless to create  interactive UI's </p>
           </div>
           <div>
-            <img src={icon1} />
+            <img src={icon3} alt="logo" />
             <h3>Declarative</h3>
             <p>  React makes it painless to create  interactive UI's </p>
           </div>
           <div>
-            <img src={icon1} />
+            <img src={icon4} alt="logo" />
             <h3>Declarative</h3>
             <p>  React makes it painless to create  interactive UI's </p>
           </div>
         </section>
+        <footer>
+          Made By IronGooglers
+        </footer>
       </div>
     )
   }
